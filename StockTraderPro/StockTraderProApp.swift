18e987b9a08b2e79c05a562da05db74aa8ac513f
MineGamer69 <aaryankapoor2003@gmail.com>
2024-02-14 15:41:08 -0500

    Initial Commit

---
+++ StockTraderPro/StockTraderProApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTraderProApp.swift
+//  StockTraderPro
+//
+//  Created by Aaryan Kapoor on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct StockTraderProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
